6f2f7f88e311654bd5b84a16d21b0d28237865eb
Ragib Asif <182296466+ragibasif@users.noreply.github.com>
2024-12-08 00:32:21 -0500

    {{BACKUP}} Sun Dec  8 00:32:21 2024

---
+++ 0001-two-sum/main.py
@@ -0,0 +1,9 @@
+# python hashmap solution
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        hashmap = {}
+        for i,j in enumerate(nums):
+            if target-j in hashmap:
+                return [hashmap[target-j],i]
+            hashmap[j] = i
+
